7c84b352a7e6d1ac746e65310c4f9148ef85b44c
Administrator <Administrator@Mrs-zhang>
2017-04-14 21:34:47 +0800

    Git study

---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String[] strs = new String[]{"abfds1", "advesd2", "dasfdsa3", "cdsaew1", "abbdsa3", "abbdsa2", "abbdsa"};  
+		/*Arrays.sort(strs);  
+		for(String str : strs) {  
+		    System.out.println(str);  
+		} */
+
+		String[] strs2 = new String[]{"c","b","c","d","y","x","g","h"};
+		Arrays.sort(strs2);  
+		for(String str : strs2) {  
+		    System.out.println(str);  
+		} 
+		
+	}
+
+}
